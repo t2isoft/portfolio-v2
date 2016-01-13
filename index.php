4d45e4ecfa12af6f513e23070ea61fcc7e8cbd05
karl92390 <karl.kelleoglu@laposte.net>
2016-01-13 10:06:02 +0100

    clean project

---
+++ index.php
@@ -0,0 +1,433 @@
+﻿<!DOCTYPE html>
+<html lang="fr-FR" class="no-js">
+	<head>
+
+		<!-- ==============================================
+		Seo
+		=============================================== -->
+
+		<div itemscope="" itemtype="http://schema.org/Person">
+	  <meta itemprop="gender" content="Male">
+	  <meta itemprop="birthDate" content="August 26, 1994">
+	  <meta itemprop="jobTitle" content="CEO of T2isoft">
+	  <meta itemprop="email" content="contact@karlkelleoglu.com">
+	  <meta itemprop="url" content="http://www.karlkelleoglu.com">
+	  <meta itemprop="memberOf" content="T2isoft">
+	  <meta itemprop="image" content="http://karlkelleoglu.com/assets/photo.jpg">
+	  <span itemprop="name">
+    <link itemprop="sameAs" href="https://github.com/t2isoft">
+    <link itemprop="sameAs" href="https://www.codeur.com/-t2isoft">
+    <link itemprop="sameAs" href="https://www.facebook.com/karl.kelleoglu">
+    <link itemprop="sameAs" href="https://www.linkedin.com/in/karlkelleoglu">
+    <link itemprop="sameAs" href="https://twitter.com/t2isoft">
+    <!--<link itemprop="sameAs" href="https://plus.google.com/103377602465276321204/posts">Karl Kelleoglu</span> is the CEO of T2isoft Inc.-->
+	</div>
+
+
+		<!-- ==============================================
+		Title and Meta Tags
+		=============================================== -->
+		<title>Karl Kelleoglu | Portfolio</title>
+		<meta name="description" content="Je suis un entrepreneur français ayant fait des études en ingénieurie informatique est travaillant avec plusieurs partenaires situées en belgique, suisse, maroc etc..">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<META NAME="Author" CONTENT="karl kelleoglu, T2isoft">
+		<META NAME="Category" CONTENT="Création de site internet, création d'application mobile">
+		<META NAME="Copyright" CONTENT="T2isoft inc.">
+		<META NAME="Language" CONTENT="fr">
+		<META NAME="Distribution" CONTENT="global">
+		<META NAME="Identifier-URL" CONTENT="www.karlkelleoglu.com">
+		<META NAME="Keywords" CONTENT="Portfolio, karl kelleoglu, Création de site internet, création d'application mobile, Création de site web, Référencement">
+		<META NAME="Publisher" CONTENT="T2isoft">
+		<META NAME="Robots" CONTENT="all">
+		<META NAME="GOOGLEBOT" CONTENT="NOARCHIVE">
+		<META http-equiv="Content-Type"	content="text/html;charset=utf-8">
+		<META http-equiv="Content-Language" content="fr">
+		<META http-equiv="Refresh"	content="300; URL=http://www.karlkelleoglu.com">
+		<META http-equiv="Pragma" CONTENT="no-cache">
+
+		<!-- ==============================================
+		Favicons
+		=============================================== -->
+		<link rel="shortcut icon" href="assets/favicon.ico">
+		<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="assets/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="assets/apple-touch-icon-114x114.png">
+
+		<!-- ==============================================
+		CSS
+		=============================================== -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/font-awesome.css">
+		<link rel="stylesheet" href="css/flexslider.css">
+		<link rel="stylesheet" href="css/designr-theme-cyan.css">
+
+
+		<!-- ==============================================
+		Fonts
+		=============================================== -->
+		<link href='http://fonts.googleapis.com/css?family=Lato:400,300,300italic,400italic,700,700italic,900' rel='stylesheet' type='text/css'>
+
+		<!-- ==============================================
+		JS
+		=============================================== -->
+
+		<!--[if lt IE 9]>
+			<script src="js/respond.min.js"></script>
+		<![endif]-->
+
+		<script type="text/javascript" src="js/libs/modernizr.min.js"></script>
+
+
+	</head>
+
+	<body data-spy="scroll" data-target="#main-nav" data-offset="200">
+
+		<!-- ==============================================
+		MAIN NAV
+		=============================================== -->
+		<div id="main-nav" class="navbar navbar-fixed-top">
+			<div class="container">
+
+				<div class="navbar-header">
+
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+					</button>
+
+					<!-- ======= LOGO ========-->
+					<a class="navbar-brand scrollto" href="#home">Karl Kelleoglu<br><span>Entrepreneur / Développeur</span></a>
+					<!-- <a class="navbar-brand scrollto ir-logo" href="#">DesignPandr<br><span>UI Designer</span></a> -->
+
+				</div>
+
+				<div id="site-nav" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="sr-only">
+							<a href="#home" class="scrollto">Home</a>
+						</li>
+						<li>
+							<a href="#services" class="scrollto">Mes services</a>
+						</li>
+						<li>
+							<a href="#about" class="scrollto">A propos de moi</a>
+						</li>
+						<!-- <li>
+							<a href="#portfolio" class="scrollto">Portfolio</a>
+						</li> -->
+						<li>
+							<a href="#contact" class="scrollto">Contactez moi</a>
+						</li>
+					</ul>
+				</div><!--End navbar-collapse -->
+
+			</div><!--End container -->
+
+		</div><!--End main-nav -->
+
+		<!-- ==============================================
+		HEADER
+		=============================================== -->
+		<header id="home" class="jumbotron">
+
+			<div class="container">
+
+				<div class="message-box clearfix">
+
+					<div class="left-col">
+						<p>Welcome, here<br><span>Create&#160Your&#160Dream</span><br/></p>
+					</div>
+
+					<div class="right-col flexslider home-slider">
+
+						<ul class="slides">
+
+							<li><p>&#160&#160&#160Apps<p></li>
+
+							<li><p>&#160&#160&#160Websites</p></li>
+
+                            <li><p>&#160&#160&#160Seo</p></li>
+
+
+						</ul>
+
+					</div><!--End home-slider -->
+
+				</div><!--End message-box -->
+
+			</div><!--End container -->
+
+		</header><!--End header -->
+
+		<!-- ==============================================
+		SERVICES
+		=============================================== -->
+		<section id="services">
+
+			<div class="container">
+
+				<h1 class="section-title scrollimation scale-in">Mes <span>Services</span> <i class="icon-wrench"></i></h1>
+
+				<div class="row services scrollimation fade-left">
+					<div class="col-md-3 text-center">
+						<a href="#fakelink" class="icon"><i class="icon-pencil"></i></a>
+						<h2>Gestion de projet</h2>
+					</div>
+					<div class="col-md-3 details">
+						<p>Vous avez besoin d'aide pour la gestion d'un de vos projets, redaction de cahier des charges, analyse fonctionnel, users case, la mise en place d'itil ou de la méthode agile.</p>
+					</div>
+					<div class="col-md-3 text-center">
+						<a href="#fakelink" class="icon"><i class="icon-code"></i></a>
+						<h2>Développement</h2>
+					</div>
+					<div class="col-md-3 details">
+						<p>Vous souhaitez crée votre site web, une application web qui permettrait à votre société de gagner en chiffre d'affaire ou tous simplement de maintenir un projet existant et de l'améliorer. Je suis force de proposition.</p>
+					</div>
+				</div><!--End row (top) -->
+
+				<div class="row line visible-md visible-lg">
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+					<div class="col-md-3"><span class="spot"></span></div>
+				</div><!--End row -->
+
+				<div class="row services bottom-row scrollimation fade-right">
+					<div class="col-md-3 text-center col-md-push-3">
+						<h2>Seo</h2>
+						<a href="#fakelink" class="icon"><i class="icon-star"></i></a>
+					</div>
+					<div class="col-md-3 details col-md-pull-3">
+						<p>Vous avez un site internet ou simplement une société et vous voulez gagner en visibilité et en part de marché à travers le plus grand marché du monde à savoir internet.</p>
+					</div>
+					<div class="col-md-3 text-center  col-md-push-3">
+						<h2>Application mobile</h2>
+						<a href="#fakelink" class="icon"><i class="icon-mobile-phone"></i></a>
+					</div>
+					<div class="col-md-3 details  col-md-pull-3">
+						<p>Vous souhaitez crée une application mobile et vous trouvez que les prix du marché sont excessif. Vous souhaiter une application sur mesure pour gagner une nouvelle clientèle.</p>
+					</div>
+				</div><!--End row (bottom)-->
+
+			</div><!--End container -->
+
+		</section><!--End services section-->
+
+		<!-- ==============================================
+		ABOUT
+		=============================================== -->
+		<section id="about">
+
+			<div class="container">
+
+				<div class="row">
+					<div class="col-sm-4 col-sm-offset-1 scrollimation fade-right">
+						<img class="img-responsive img-circle img-center" src="assets/photo.jpg" alt="">
+					</div>
+					<div class="col-sm-6 col-sm-offset-1 scrollimation fade-left">
+						<h1>Bonjour, je suis ici pour vous aider ...</h1>
+						<p>Je suis un chef de projet vivant à<a href="#linktomaps"> Paris, France</a>. J'ai travaillé avec des sociétés comme <a href="#somelink">BDM Service et transport</a>, des sociétés d'évenementiel tels que <a href="#somelink">City one</a> et des société de courtage en assurance tels que <a href="#somelink">Capi finance</a> et bien sur j'ai monté ma propre société <a href="http://t2isoft.com/" target="_blank">T2isoft</a>.<br/>Créer n'est pas juste un travail pour moi, c'est ma passion. </p>
+						<!-- <a href="#portfolio" class="btn btn-theme transparent scrollto">View my Works</a> -->
+					</div>
+				</div><!--End row -->
+
+			</div><!--End container -->
+
+		</section><!--End about section -->
+
+		<!-- ==============================================
+		SKILLS
+		=============================================== -->
+		<section id="skills">
+
+			<div class="container">
+
+				<div class="row skills">
+
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="80"><span class="percent">80</span></span>
+						<h2 class="text-center">Gestion de projet</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="70"><span class="percent">70</span></span>
+						<h2 class="text-center">Création de site internet</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="60"><span class="percent">60</span></span>
+						<h2 class="text-center">Seo</h2>
+					</div>
+					<div class="col-sm-6 col-md-3 text-center">
+						<span class="chart" data-percent="90"><span class="percent">90</span></span>
+						<h2 class="text-center">Application mobile</h2>
+					</div>
+
+				</div><!--End row -->
+
+			</div><!--End container -->
+
+		</section><!--End skills section -->
+
+
+
+		<!-- ==============================================
+		TWITTER FEED
+		=============================================== -->
+		<!-- <section id="twitter">
+			<div class="container">
+				<div class="row scrollimation fade-in">
+					<div class="col-sm-1 twitter-icon">
+						<i class="icon-twitter"></i>
+					</div>
+					<div class="col-sm-11">
+						<div id="twitter-slider" class="flexslider" data-widget-id="345690956013633536" data-tweets-length="5">
+
+						</div>
+					</div>
+				</div><!-- End row -->
+			</div><!-- End container -->
+
+		</section><!-- End Twitter section -->
+
+		<!-- ==============================================
+		CONTACT
+		=============================================== -->
+		<section  id="contact">
+
+			<div class="container">
+
+				<h1 class="section-title scrollimation scale-in"> <span>Keep calm</span> and contact me</h1>
+
+				<div class="row">
+
+					<div class="col-sm-5 contact-info scrollimation fade-right">
+
+
+						<address>
+							Karl KELLEOGLU<br>
+							8 Rue gaston appert<br>
+							Villeneuve la garenne, 92390<br>
+							contact@karlkelleoglu.com<br>
+							06-59-98-44-86
+						</address>
+
+						<ul class="socials">
+							<li><a href="https://twitter.com/t2isoft" target=_blank><i class="icon-twitter"></i></a></li>
+							<li><a href="https://www.facebook.com/karl.kelleoglu" target=_blank><i class="icon-facebook"></i></a></li>
+							<!-- <li><a href="#fakelink" target=_blank><i class="icon-dribbble"></i></a></li> -->
+							<li><a href="https://plus.google.com/103377602465276321204/posts" target=_blank><i class="icon-google-plus"></i></a></li>
+							<li><a href="http:\\fr.linkedin.com/pub/karl-kelleoglu/57/7a4/9ab/" target=_blank><i class="icon-linkedin"></i></a></li>
+							<li><a href="https://github.com/t2isoft" target=_blank><i class="icon-github"></i></a></li>
+							<li><a href="https://www.codeur.com/-t2isoft" target=_blank><i class="icon-star"></i></a></li>
+						</ul>
+
+					</div>
+
+					<!--=== Contact Form ===-->
+
+					<form id="contact-form" class="col-sm-7 scrollimation fade-left" action="contact.php" method="post" novalidate>
+
+						<div class="form-group">
+						  <label class="control-label" for="contact-name">Name</label>
+						  <div class="controls">
+							<i class="icon-user"></i>
+							<input id="contact-name" name="contactName" placeholder="Nom" class="form-control input-lg requiredField" type="text" data-error-empty="Please enter your name">
+						  </div>
+						</div><!-- End name input -->
+
+						<div class="form-group">
+						  <label class="control-label" for="contact-mail">Email</label>
+						  <div class=" controls">
+							<i class="icon-envelope"></i>
+							<input id="contact-mail" name="email" placeholder="Mail" class="form-control input-lg requiredField" type="email" data-error-empty="Please enter your email" data-error-invalid="Invalid email address">
+						  </div>
+						</div><!-- End email input -->
+
+						<div class="form-group">
+						  <label class="control-label" for="contact-message">Message</label>
+							<div class="controls">
+								<i class="icon-comment"></i>
+								<textarea id="contact-message" name="comments"  placeholder="Message" class="form-control input-lg requiredField" rows="5" data-error-empty="Please enter your message"></textarea>
+							</div>
+						</div><!-- End textarea -->
+
+						<p><button name="submit" type="submit" class="btn btn-theme btn-lg" data-error-message="Error!" data-sending-message="Envoi..." data-ok-message="Message Envoyer"><i class="icon-location-arrow"></i>Envoyer</button></p>
+						<input type="hidden" name="submitted" id="submitted" value="true" />
+
+					</form><!-- End contact-form -->
+
+				</div><!-- End row -->
+
+			</div><!-- End container -->
+
+		</section><!-- End contact section -->
+
+		<!-- ==============================================
+		FOOTER
+		=============================================== -->
+		<footer id="main-footer">
+
+			<div class="container">
+
+				<div class="row">
+
+					<div class="col-sm-6 left-col">
+						<h1 class="small-logo">Karl KELLEOGLU<br><span>Chef de projet / Développeur</span></h1>
+						<ul class="footer-nav">
+							<li><a class="scrollto" href="#home">Home</a></li>
+							<li><a class="scrollto" href="#services">Services</a></li>
+							<!-- <li><a class="scrollto" href="#portfolio">Portfolio</a></li> -->
+							<li><a class="scrollto" href="#contact">Contact</a></li>
+						</ul>
+						<p>Copyright © <SCRIPT language=javascript>
+						var ladate=new Date()
+						document.write(ladate.getFullYear())
+						</SCRIPT>  <a	href="http://t2isoft.com/">T2isoft.com</a>
+
+
+
+															</br>Numéro SIREN / SIRET : 80143037200012
+														</p>
+
+					</div>
+
+					<div class="col-sm-6 right-col">
+						<h2>Qui suis-je ?</h2>
+						<p>Je suis un entrepreneur français ayant fait des études en ingénieurie informatique est travaillant avec plusieurs partenaires situées en belgique, suisse, maroc etc...</p>
+					</div>
+
+				</div><!-- End row -->
+
+			</div><!-- End container -->
+
+		</footer><!-- End footer -->
+
+		<!-- ==============================================
+		SCRIPTS
+		=============================================== -->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/libs/jquery-1.8.2.min.js">\x3C/script>')</script>
+
+		<script src="js/libs/bootstrap.min.js"></script>
+		<script src='js/jquery.scrollto.js'></script>
+		<script src='js/jquery.flexslider.min.js'></script>
+		<script src='js/twitterFetcher_v10_min.js'></script>
+		<script src='js/jquery.masonry.min.js'></script>
+		<script src="js/waypoints.min.js"></script>
+		<script src="js/jquery.easypiechart.js"></script>
+		<script src="js/jquery.backstretch.min.js"></script>
+		<script src="js/contact.js"></script>
+		<script src="js/designr.js"></script>
+		<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-64399634-1', 'auto');
+	  ga('send', 'pageview');
+
+		</script>
+
+	</body>
+
+</html>
